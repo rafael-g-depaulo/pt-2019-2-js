372a23c064520541a22da14c4ac0f7d90c53aef0
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2019-10-05 12:25:17 -0300

    tabela pronta?

--- js-pratica/ジャバスクリプトちゃん.js
+++ js-pratica/ジャバスクリプトちゃん.js
@@ -1,8 +1,54 @@
-const tableLines = Array
-  .from( document
-    .querySelector("#minha-linda-tabela")
-    .rows
-  )
+// informação a ser inserida na tabela
+const tableInfo = [
+  {
+    nome: 'sapo de chocolate (1 unidade)',
+    tipo: 'comida',
+    valor: -15.50,
+  },
+  {
+    nome: 'prêmio da competição de melhor animorfo',
+    tipo: 'prêmio',
+    valor: 28.8,
+  },
+  {
+    nome: 'Varinha Nova',
+    tipo: 'utilitário',
+    valor: -69.69,
+  },
+  {
+    nome: 'Foto da Focátia que se mexe',
+    tipo: 'fofo',
+    valor: -88.88,
+  },
+  {
+    nome: 'Salário do estágio na loja de doces mágicos',
+    tipo: 'salário',
+    valor: 450.00,
+  },
+  {
+    nome: 'Nimbus 2000',
+    tipo: 'vroom vroom',
+    valor: -199.90,
+  },
+  {
+    nome: 'Caixa de Feijões de Todos os Sabores (250g)',
+    tipo: 'comida',
+    valor: -10.50,
+  }
+]
 
-tableLines
-  .forEach((line, i) => line.firstElementChild.innerText = i + 1)
+// pegando uma referência ao corpo da tabela (onde inserir os itens)
+const tbody = document.querySelector("#minha-linda-tabela > tbody")
+
+// criando o HTML correspondente
+tableInfo
+  .map((item, id) => `
+    <tr>
+      <th scope="row">${id+1}</th>
+      <td>${item.nome}</td>
+      <td>${item.tipo}</td>
+      <td>${item.valor > 0 ? "" : "- "}R$${Math.abs(item.valor.toFixed(2))}<t>
+    </tr>
+  `)
+  // inserindo o html na posição certa
+  .forEach(item => tbody.insertAdjacentHTML('beforeend', item))
